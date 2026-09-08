082ec0e0282da640a3b0c6121c58063bc6db37b0
Elian Trizotti <trizolino2@gmail.com>
2026-09-08 08:10:49 -0300

    Finished music player implementation

--- scripts.js
+++ scripts.js
@@ -3,19 +3,22 @@ let songs = [
         name: "Have a Nice Day",
         artist: "Bon Jovi",
         image: "/resources/images/Have_a_Nice_Day.jpg",
-        location: "/resources/songs/HAVEANICEDAY.mp3"
+        location: "/resources/songs/HAVEANICEDAY.mp3",
+        liked: false
     },
     {
         name: "Send Me An Angel",
         artist: "Scorpions",
         image: "/resources/images/ScorpionsCrazyWorld.jpg",
-        location: "/resources/songs/Scorpions - Send Me An Angel.mp3"
+        location: "/resources/songs/Scorpions - Send Me An Angel.mp3",
+        liked: false
     },
     {
         name: "Rock you like a Hurricane",
         artist: "Scorpions",
         image: "/resources/images/The_Scorpions_LatFS.jpg",
-        location: "/resources/songs/Scorpions - Rock you like a Hurricane.mp3"
+        location: "/resources/songs/Scorpions - Rock you like a Hurricane.mp3",
+        liked: false
     }
 ];
 
@@ -23,6 +26,10 @@ const songName = document.getElementById("song-name");
 const artistName = document.getElementById("artist-name");
 const cover = document.getElementById("cover");
 const audio = document.getElementById("audio");
+const progress = document.getElementById("progress")
+const progressContainer = document.getElementById("progress-container");
+const songTime = document.getElementById("song-time");
+const totalTime = document.getElementById("total-time");
 
 songName.innerText = songs[0].name;
 artistName.innerText = songs[0].artist;
@@ -30,9 +37,56 @@ cover.src = songs[0].image;
 audio.src = songs[0].location;
 
 let playing = false;
+let sortedPlaylist = [...songs];
 let repeatSwitch = false;
 let shuffleSwitch = false;
 let playQueueIndex = 0;
+
+function nextSong() {
+    if (sortedPlaylist[playQueueIndex + 1]) {
+        playQueueIndex += 1;
+        playSong();
+    } else {
+        playQueueIndex = 0;
+        songName.innerText = sortedPlaylist[playQueueIndex].name;
+        artistName.innerText = sortedPlaylist[playQueueIndex].artist;
+        cover.src = sortedPlaylist[playQueueIndex].image;
+        audio.src = sortedPlaylist[playQueueIndex].location;
+        if (repeatSwitch) {
+            playSong();
+        } else {
+            audio.pause();
+        }
+    }
+}
+
+function shuffleArray(preShuffledArray) {
+    let size = preShuffledArray.length;
+    let currentIndex = size - 1;
+    while (currentIndex > 0) {
+        let randomIndex = Math.floor(Math.random() * size);
+        let aux = preShuffledArray[currentIndex];
+        preShuffledArray[currentIndex] = preShuffledArray[randomIndex];
+        preShuffledArray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
+
+function toHHMMSS(number) {
+    let hours = Math.floor(number / 3600);
+    let minutes = Math.floor((number - hours * 3600) / 60);
+    let seconds = Math.floor(number - hours * 3600 - minutes * 60);
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+};
+
+function playSong() {
+    audio.pause();
+    songName.innerText = sortedPlaylist[playQueueIndex].name;
+    artistName.innerText = sortedPlaylist[playQueueIndex].artist;
+    cover.src = sortedPlaylist[playQueueIndex].image;
+    audio.src = sortedPlaylist[playQueueIndex].location;
+    audio.play();
+}
 
 play.addEventListener("click", () => {
     if (playing) {
@@ -50,47 +104,79 @@ play.addEventListener("click", () => {
 });
 
 next.addEventListener('click', () => {
-
-    if (songs[playQueueIndex + 1]) {
-        playQueueIndex += 1;
-        playSong();
-    } else {
-        playQueueIndex = 0;
-        songName.innerText = songs[playQueueIndex].name;
-        artistName.innerText = songs[playQueueIndex].artist;
-        cover.src = songs[playQueueIndex].image;
-        audio.src = songs[playQueueIndex].location;
-        console.log(repeatSwitch);
-        if (repeatSwitch) {
-            playSong(); 
-        } else {
-        audio.pause();
-        }
-    }
+    nextSong();    
 });
 
 previous.addEventListener('click', () => {
-    playQueueIndex -= 1;
+    if (playQueueIndex === 0) {
+        playQueueIndex = sortedPlaylist.length - 1;
+    } else {
+        playQueueIndex -= 1;
+    }
     playSong();
+    if(sortedPlaylist[playQueueIndex].liked) {
+        like.querySelector(".bi").classList.remove("bi-heart");
+        like.querySelector(".bi").classList.add("bi-heart-fill");
+    }
+    else{
+        like.querySelector(".bi").classList.remove("bi-heart-fill");
+        like.querySelector(".bi").classList.add("bi-heart");
+    }
 });
 
 repeat.addEventListener('click', () => {
     let repeatButton = document.getElementById('repeat');
     if (repeatSwitch) {
-        repeatButton.classList.remove('green-button');
+        repeatButton.classList.remove('active-button');
         repeatSwitch = false;
     }
     else {
-        repeatButton.classList.add('green-button');
+        repeatButton.classList.add('active-button');
         repeatSwitch = true;
     }
 });
 
-function playSong() {
-    audio.pause();
-    songName.innerText = songs[playQueueIndex].name;
-    artistName.innerText = songs[playQueueIndex].artist;
-    cover.src = songs[playQueueIndex].image;
-    audio.src = songs[playQueueIndex].location;
-    audio.play();
-}
+shuffle.addEventListener('click', () => {
+    if (!shuffleSwitch) {
+        shuffleSwitch = true;
+        shuffleArray(sortedPlaylist);
+        shuffle.classList.add('active-button');
+    } else {
+        shuffleSwitch = false;
+        sortedPlaylist = [...songs];
+        shuffle.classList.remove('active-button');
+    }
+
+})
+
+audio.addEventListener('timeupdate', () => {
+    const barWidth = (audio.currentTime / audio.duration) * 100;
+    progress.style.setProperty('--progress', `${barWidth}%`);
+    songTime.innerText = toHHMMSS(audio.currentTime);
+});
+audio.addEventListener('ended', nextSong);
+audio.addEventListener('loadedmetadata', () => {
+    totalTime.innerText = toHHMMSS(audio.duration);
+})
+
+progressContainer.addEventListener('click', (event) => {
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition / width) * audio.duration;
+    audio.currentTime = jumpToTime;
+P});
+
+like.addEventListener('click', () => {
+    let likeButton = document.getElementById('like');
+
+    if (songs[playQueueIndex].liked) {
+        likeButton.querySelector(".bi").classList.remove("bi-heart-fill");
+        likeButton.querySelector(".bi").classList.add("bi-heart");
+        songs[playQueueIndex].liked = false;
+    }
+    else {
+        likeButton.querySelector(".bi").classList.remove("bi-heart");
+        likeButton.querySelector(".bi").classList.add("bi-heart-fill");
+        songs[playQueueIndex].liked = true;
+    }
+});
